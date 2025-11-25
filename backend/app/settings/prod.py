d5de32739bfc05a07a308e842d70f2b89c7f0bc4
Imran Hoshain <iforuimran@gmail.com>
2025-11-25 14:09:45 +0600

    Update allow host

--- backend/app/settings/prod.py
+++ backend/app/settings/prod.py
@@ -10,9 +10,8 @@ from app.settings.base import *
 DEBUG = False
 
 # ---- ALLOWED_HOSTS from environment ----
-#raw_allowed_hosts = os.getenv("DJANGO_ALLOWED_HOSTS", "")
-#ALLOWED_HOSTS = [h.strip() for h in raw_allowed_hosts.split(",") if h.strip()]
-ALLOWED_HOSTS = [*]
+raw_allowed_hosts = os.getenv("DJANGO_ALLOWED_HOSTS", "")
+ALLOWED_HOSTS = [h.strip() for h in raw_allowed_hosts.split(",") if h.strip()]
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
