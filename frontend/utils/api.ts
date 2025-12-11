4642227a981ea65614f9f224138a570a5b6f71bf
programmerrm <programmerwebrm@gmail.com>
2025-12-11 12:25:13 +0600

    fall back domain name add

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,5 +1,15 @@
-export const MEDIA_URL = process.env.NEXT_PUBLIC_MEDIA_URL;
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
-export const REVALIDATE_SECRET = process.env.REVALIDATE_SECRET;
-export const DOMAIN_NAME = process.env.NEXT_PUBLIC_DOMAIN_NAME;
-export const ROW_DOMAIN_NAME = process.env.NEXT_PUBLIC_ROW_DOMAIN_NAME;
+
+const FALLBACK_DOMAIN = "https://pngpoint.com";
+
+export const DOMAIN_NAME =
+    process.env.NEXT_PUBLIC_DOMAIN_NAME || FALLBACK_DOMAIN;
+
+export const MEDIA_URL =
+    process.env.NEXT_PUBLIC_MEDIA_URL || DOMAIN_NAME;
+
+export const SERVER_URL =
+    process.env.NEXT_PUBLIC_SERVER_URL || `${DOMAIN_NAME}/api/v1`;
+
+export const ROW_DOMAIN_NAME =
+    process.env.NEXT_PUBLIC_ROW_DOMAIN_NAME || "pngpoint.com";
+
