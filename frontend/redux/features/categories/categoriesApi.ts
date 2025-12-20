f1d6b400f0ad5ca00a674db8b00216a73d25a1dc
osmangoni255 <osmangoni255@gmail.com>
2025-12-20 18:36:20 +0600

    fixed urls issue

--- frontend/redux/features/categories/categoriesApi.ts
+++ frontend/redux/features/categories/categoriesApi.ts
@@ -4,7 +4,7 @@ export const categoriesApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         addCategory: builder.mutation({
             query: (formData) => ({
-                url: '/images/categories/',
+                url: '/images/categories',
                 method: 'POST',
                 body: formData,
             }),
@@ -13,30 +13,30 @@ export const categoriesApi = apiSlice.injectEndpoints({
         getCategories: builder.query({
             query: (search?: string) => {
                 const params = search ? `?name=${encodeURIComponent(search)}` : '';
-                return `/images/categories/${params}`;
+                return `/images/categories${params}`;
             },
         }),
         getSingleCategories: builder.query({
-            query: (id) => `/images/categories/${id}/`
+            query: (id) => `/images/categories/${id}`
         }),
 
         updateCategory: builder.mutation({
             query: ({ id, formData }: { id: number; formData: FormData }) => ({
-                url: `/images/categories/${id}/`,
+                url: `/images/categories/${id}`,
                 method: 'PUT',
                 body: formData,
             }),
         }),
         partialUpdateCategory: builder.mutation({
             query: ({ id, formData }: { id: number; formData: FormData }) => ({
-                url: `/images/categories/${id}/`,
+                url: `/images/categories/${id}`,
                 method: 'PATCH',
                 body: formData,
             }),
         }),
         deleteCategory: builder.mutation({
             query: (id: number) => ({
-                url: `/images/categories/${id}/`,
+                url: `/images/categories/${id}`,
                 method: 'DELETE',
             }),
         }),
