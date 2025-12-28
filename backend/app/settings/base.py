c0ceca7124fde8c0221b8110b1ffe7f628cf9ad4
osmangoni255 <osmangoni255@gmail.com>
2025-12-28 22:04:50 +0600

    fixed error in settings

--- backend/app/settings/base.py
+++ backend/app/settings/base.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
 
     'corsheaders',
     'rest_framework',
