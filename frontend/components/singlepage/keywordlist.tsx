e6ecff3b3c0122e12dcd419c89b224154a0fdd5b
osmangoni255 <osmangoni255@gmail.com>
2025-12-30 01:46:26 +0600

    fixed  redirect issue when click on keyword

--- frontend/components/singlepage/keywordlist.tsx
+++ frontend/components/singlepage/keywordlist.tsx
@@ -4,6 +4,7 @@
 import { setKeyword } from "@/redux/features/getImages/getImageSlice";
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
 
 interface ImageProps {
     image: any;
@@ -11,16 +12,26 @@ interface ImageProps {
 
 export const KeywordList: React.FC<ImageProps> = ({ image }) => {
     const dispatch = useDispatch();
+    const router = useRouter();
 
     const handleTagClick = (keyword: string) => {
+        // Set keyword in Redux store
         dispatch(setKeyword(keyword));
+        
+        // Redirect to homepage with keyword parameter
+        router.push(`/?keyword=${encodeURIComponent(keyword)}`);
     };
 
     return (
         <div className="flex flex-row flex-wrap items-center gap-1.5">
             {image?.image?.keywords?.map((item: any) => {
                 return (
-                    <button className="bg-[#eaeaea] text-black/80 rounded py-1.5 px-1.5 cursor-pointer" key={item.id} onClick={() => handleTagClick(item.slug)} type="button">
+                    <button 
+                        className="bg-[#eaeaea] hover:bg-[#d5d5d5] text-black/80 rounded py-1.5 px-1.5 cursor-pointer transition-colors" 
+                        key={item.id} 
+                        onClick={() => handleTagClick(item.slug)} 
+                        type="button"
+                    >
                         <span className="text-xs">{item.name}</span>
                     </button>
                 );
