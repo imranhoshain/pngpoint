203bd6e58e91bd0152e39531265d8e5c7b07e613
osmangoni255 <osmangoni255@gmail.com>
2026-01-04 23:53:57 +0600

    added images selection with shift in pending images page

--- frontend/redux/features/images/pendingApi.ts
+++ frontend/redux/features/images/pendingApi.ts
@@ -5,13 +5,13 @@ import { apiSlice } from '../api/apiSlice';
 export const pendingApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getPendingImages: builder.query<{ images: ImagesResponse[]; count: number }, { searchTerm: string; pageNumber: number }>({
-            query: ({ searchTerm, pageNumber }) => `/images/pending/?title=${encodeURIComponent(searchTerm)}&page=${pageNumber}`,
+            query: ({ searchTerm, pageNumber }) => `/images/pending/?keyword=${encodeURIComponent(searchTerm)}&page=${pageNumber}`,
         }),
         getPendingImagesLength: builder.query<any, void>({
             query: () => '/images/pending-images-length/',
         }),
         getUserPendingImages: builder.query<{ images: ImagesResponse[]; count: number }, { searchTerm: string; pageNumber: number }>({
-            query: ({ searchTerm, pageNumber }) => `/images/user/pending/?title=${encodeURIComponent(searchTerm)}&page=${pageNumber}`,
+            query: ({ searchTerm, pageNumber }) => `/images/user/pending/?keyword=${encodeURIComponent(searchTerm)}&page=${pageNumber}`,
         }),
         getUserPendingImagesLength: builder.query<any, void>({
             query: () => '/images/user/pending-images-length/',
