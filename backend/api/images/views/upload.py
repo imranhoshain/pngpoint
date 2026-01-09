e3cef8b7ddc8eec64443535164e442effbce9d53
abir2776 <osmangoni255@gmail.com>
2026-01-09 13:42:56 +0600

    added testing code

--- backend/api/images/views/upload.py
+++ backend/api/images/views/upload.py
@@ -82,6 +82,9 @@ class ImagesUploadAPIView(APIView):
 
         for i, image_file in enumerate(images):
             try:
+                return Response({
+                    "is_entered":True
+                }, status=200)
                 temp_path = None
                 try:
                     title = titles[i].strip()
