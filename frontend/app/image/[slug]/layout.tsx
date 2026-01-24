01aec3d6450a6bc28ee63a183b58d1f804add57d
osmangoni255 <osmangoni255@gmail.com>
2026-01-24 20:13:34 +0600

    added keywords in metadata

--- frontend/app/image/[slug]/layout.tsx
+++ frontend/app/image/[slug]/layout.tsx
@@ -54,7 +54,9 @@ export async function generateMetadata(
     return {
       title: `${data.title || 'Image'} - PNGPoint`,
       description: `Download high-quality ${data.description || 'image'} PNG with a transparent background, free to use for personal or commercial projects. Explore more related PNG images below—perfect for design, presentations, social media posts, and more.` || 'PNGPoint image details',
-      keywords:data.keywords,
+      keywords:typeof data.keywords === 'string' 
+              ? data.keywords.split(",").map((k: string) => k.trim()).filter(Boolean)
+              : data.keywords,
       alternates: {
         canonical: getImageUrl(slug),
       },
