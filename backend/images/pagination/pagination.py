036f734cd5fbb1a8cf5c05f05fb02938968db936
abir2776 <osmangoni255@gmail.com>
2026-02-01 10:21:24 +0600

    showed 20 image in home page

--- backend/images/pagination/pagination.py
+++ backend/images/pagination/pagination.py
@@ -21,9 +21,9 @@ class ImagesPagination(PageNumberPagination):
 
 
 class HomePageImagesPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 20
     page_size_query_param = "page_size"
-    max_page_size = 100
+    max_page_size = 20
 
     def get_paginated_response(self, data):
         return Response(
