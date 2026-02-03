2411f86776ee8d780fb76928da52ea6ef0f5eede
abir2776 <osmangoni255@gmail.com>
2026-02-03 17:29:00 +0600

    showed all footer content in center

--- frontend/components/footer/footer.tsx
+++ frontend/components/footer/footer.tsx
@@ -11,10 +11,10 @@ export default function Footer() {
         <footer className="relative top-0 left-0 right-0 py-10 lg:pt-16 pb-10 w-full text-white bg-[#00bcd4]">
             <div className="max-w-screen-2xl container mx-auto px-2.5 lg:px-5 w-full">
                 <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-10 w-full">
-                    <div className="flex flex-col flex-wrap gap-y-4 md:gap-y-2 w-full">
+                    <div className="flex flex-col flex-wrap gap-y-4 md:gap-y-2 w-full items-center text-center">
                         <FooterLogo />
                         <p className="text-xs xl:text-sm font-medium uppercase">free download transparent<br />png files no copyright</p>
-                        <div className="flex flex-row flex-wrap items-center gap-x-3.5 md:gap-x-8 mt-2.5">
+                        <div className="flex flex-row flex-wrap items-center justify-center gap-x-3.5 md:gap-x-8 mt-2.5">
                             <Link href={"https://x.com/pngbay"} target="_blank">
                                 <FaXTwitter className="text-xl xl:text-[22px]" />
                             </Link>
@@ -46,42 +46,42 @@ export default function Footer() {
                             </li>
                         </ul>
                     </div> */}
-                    <div className="flex flex-col flex-wrap w-full">
+                    <div className="flex flex-col flex-wrap w-full items-center text-center">
                         <h4 className="text-base lg:text-lg xl:text-xl font-semibold uppercase">Company</h4>
-                        <ul className="flex flex-col flex-wrap gap-y-2.5 md:gap-y-3 mt-2.5 md:mt-5 ml-4.5">
-                            <li className=" list-disc">
+                        <ul className="flex flex-col flex-wrap gap-y-2.5 md:gap-y-3 mt-2.5 md:mt-5">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/about/"}>about us</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/contact/"}>contact</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/terms/"}>terms</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/privacy/"}>privacy</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/license/"}>license</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/dmca/"}>dmca</Link>
                             </li>
                         </ul>
                     </div>
-                    <div className="flex flex-col flex-wrap w-full">
+                    <div className="flex flex-col flex-wrap w-full items-center text-center">
                         <h4 className="text-base lg:text-lg xl:text-xl font-semibold uppercase">Community</h4>
-                        <ul className="flex flex-col flex-wrap gap-y-2.5 md:gap-y-3 mt-2.5 md:mt-5 ml-4.5">
-                            <li className=" list-disc">
+                        <ul className="flex flex-col flex-wrap gap-y-2.5 md:gap-y-3 mt-2.5 md:mt-5">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"https://x.com/pngbay"} target="_blank">x</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"https://www.pinterest.com/pngbay"} target="_blank">pinterest</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"https://www.instagram.com/pngba_y"} target="_blank">instagram</Link>
                             </li>
-                            <li className=" list-disc">
+                            <li className="list-none">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"https://www.facebook.com/pngbay"} target="_blank">facebook</Link>
                             </li>
                         </ul>
