eb805fbf3c1efece03818a1907eb3e9069fcc6b8
abir2776 <osmangoni255@gmail.com>
2026-02-03 11:22:52 +0600

    fixed footer text color

--- frontend/components/footer/footer.tsx
+++ frontend/components/footer/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     const currentYear = getCurrentYear();
     const { FaXTwitter, FaFacebook, FaPinterest, FaInstagram } = ReactIcons;
     return (
-        <footer className="relative top-0 left-0 right-0 py-10 lg:pt-16 pb-10 w-full text-[#0077a2] bg-[#0077a2]">
+        <footer className="relative top-0 left-0 right-0 py-10 lg:pt-16 pb-10 w-full text-white bg-[#0077a2]">
             <div className="max-w-screen-2xl container mx-auto px-2.5 lg:px-5 w-full">
                 <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-10 w-full">
                     <div className="flex flex-col flex-wrap gap-y-4 md:gap-y-2 w-full">
@@ -29,23 +29,23 @@ export default function Footer() {
                             </Link>
                         </div>
                     </div>
-                    <div className="flex flex-col flex-wrap w-full">
+                    {/* <div className="flex flex-col flex-wrap w-full">
                         <h4 className="text-base lg:text-lg xl:text-xl font-semibold uppercase">Resources</h4>
                         <ul className="flex flex-col flex-wrap gap-y-2.5 md:gap-y-3 mt-2.5 md:mt-5 ml-4.5">
-                            {/* <li className=" list-disc">
+                            <li className=" list-disc">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/categories/"}>categories</Link>
                             </li>
                             <li className=" list-disc">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/sub-categories/"}>sub categories</Link>
-                            </li> */}
-                            {/* <li className=" list-disc">
+                            </li>
+                            <li className=" list-disc">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/user/login/"}>contributor login</Link>
                             </li>
                             <li className=" list-disc">
                                 <Link className="block w-fit text-sm xl:text-base font-normal uppercase" href={"/user/register/"}>contributor register</Link>
-                            </li> */}
+                            </li>
                         </ul>
-                    </div>
+                    </div> */}
                     <div className="flex flex-col flex-wrap w-full">
                         <h4 className="text-base lg:text-lg xl:text-xl font-semibold uppercase">Company</h4>
                         <ul className="flex flex-col flex-wrap gap-y-2.5 md:gap-y-3 mt-2.5 md:mt-5 ml-4.5">
