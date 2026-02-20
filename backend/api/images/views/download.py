bacb14700eea70937470388be4d1f30e881fd7db
osmangoni255 <osmangoni255@gmail.com>
2026-02-21 01:20:12 +0600

    fixed image download issue

--- backend/api/images/views/download.py
+++ backend/api/images/views/download.py
@@ -58,7 +58,7 @@ class DownloadImageViewSet(viewsets.ViewSet):
         }
         variant = size_variant_map.get(size, 'singleimage')
         
-        image_url = f"https://{config.images_domain}/{config.account_hash}/{image_id}/{variant}"
+        image_url = f"https://{config.images_domain}/{config.account_hash}/{image_id}/singleimage"
 
         try:
             response = requests.get(image_url, stream=True)
