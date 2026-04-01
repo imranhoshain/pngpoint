d40209ec0bd18f165faa69ceba2b20d4e4edbb63
abir2776 <osmangoni255@gmail.com>
2026-04-01 14:08:07 +0600

    revert code

--- frontend/components/homepageMainComponent/homepageMainComponent.tsx
+++ frontend/components/homepageMainComponent/homepageMainComponent.tsx
@@ -198,7 +198,7 @@ export const HomepageMainComponent = ({ initialImagesData }: { initialImagesData
              * The min-h-[52px] reserves the space pagination occupies.
              */}
             <div className="min-h-[52px]">
-                {imagesData?.count > 20 && (
+                {imagesData?.count > 48 && (
                     <TrendingImagesPagination count={imagesData?.count} />
                 )}
             </div>
