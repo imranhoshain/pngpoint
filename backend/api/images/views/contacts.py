005259ed28b9ea4b5b27c069b219df099504f442
osman <osmangoni255@gmail.com>
2026-04-02 19:09:09 +0600

    remove testing code

--- backend/api/images/views/contacts.py
+++ backend/api/images/views/contacts.py
@@ -29,7 +29,7 @@ class ContactListCreateAPIView(ListCreateAPIView):
                     f"Message:\n{contact.message}"
                 ),
                 from_email='robiulcc@gmail.com',
-                recipient_list=['osmangoni255@gmail.com'],
+                recipient_list=['robiulcc@gmail.com'],
                 fail_silently=False,
             )
         except Exception as e:
