1e5800025a044106c149affa0b5aabe865f510f8
osman <osmangoni255@gmail.com>
2026-04-12 18:16:16 +0600

    fixed double content issue in contacts page

--- frontend/app/contact/layout.tsx
+++ frontend/app/contact/layout.tsx
@@ -23,9 +23,9 @@ export default function ContactRootLayout({
 }>) {
     return (
         <section className="relative top-0 left-0 right-0 w-full h-screen">
-            <Header />
+            {/* <Header /> */}
             {children}
-            <Footer />
+            {/* <Footer /> */}
         </section>
     );
 }
