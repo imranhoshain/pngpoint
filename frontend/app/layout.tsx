6a773770b9520bac6b2ea31eaff7e44b1aeeb86e
osman <osmangoni255@gmail.com>
2026-04-15 15:07:31 +0600

    fixed tag manager code

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -100,7 +100,7 @@ export default function RootLayout({
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-                        gtag('config', 'G-0D66LNPY7G');
+                        gtag('config', 'G-TRJEV69JRZ');
                     `}
                 </Script>
 
